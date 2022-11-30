c2ea159fd5261560ca9be9b684c430e560a87596
budzik12 <81246476+budzik12@users.noreply.github.com>
2022-11-30 09:16:23 +0100

    dodanie listy miast do sppinnerów

--- app/src/main/java/com/nkodem/a23_11/MainActivity.kt
+++ app/src/main/java/com/nkodem/a23_11/MainActivity.kt
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 
     
