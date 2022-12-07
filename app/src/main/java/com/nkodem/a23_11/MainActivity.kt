40fc1535113d82846bc40be3223aa425679db57d
budzik12 <81246476+budzik12@users.noreply.github.com>
2022-12-07 09:20:24 +0100

    losowy dystans

--- app/src/main/java/com/nkodem/a23_11/MainActivity.kt
+++ app/src/main/java/com/nkodem/a23_11/MainActivity.kt
@@ -3,14 +3,24 @@ package com.nkodem.a23_11
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Spinner
+import android.widget.Button
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rarity = resources.getStringArray(R.array.dystans)
+        val randomRarity = rarity.random()
+        val zmien = findViewById(R.id.zmien) as Button
+        val dystans = findViewById(R.id.dystans) as TextView
+
+        zmien.setOnClickListener {
+                dystans.setText(randomRarity)
+        }
+
         fun main(ards: Array<String>){
             val rows = 2
             val columns = 2
